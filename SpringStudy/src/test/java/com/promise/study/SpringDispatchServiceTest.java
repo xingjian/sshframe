9550378accc16fa0a366bc00cacbfd771d356245
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2011-09-16 06:11:01 +0000

    git-svn-id: svn://10.10.191.1:3690/sshframe/trunk@257 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ SpringStudy/src/test/java/com/promise/study/SpringDispatchServiceTest.java
@@ -0,0 +1,26 @@
+/*@文件名: SpringDispatchServiceTest.java  @创建人: 邢健   @创建日期: 2011-9-16 下午1:51:37*/
+package com.promise.study;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**   
+ * @类名: SpringDispatchServiceTest.java 
+ * @包名: com.promise.study 
+ * @描述: 调度服务测试用例
+ * @作者: 邢健 xingjian@yeah.net   
+ * @日期: 2011-9-16 下午1:51:37 
+ * @版本 V1.0   
+ */
+public class SpringDispatchServiceTest {
+
+	/**
+	 * 测试注解方式spring调度
+	 */
+	@Test
+	public void testSpringDispatchByAnnotation() {
+		fail("Not yet implemented");
+	}
+
+}
