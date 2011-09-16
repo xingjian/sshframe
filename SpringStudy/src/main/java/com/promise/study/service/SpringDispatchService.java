a5fefbff0dec2b888649b309483d3ccfabdf2ff0
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2011-09-16 06:10:31 +0000

    git-svn-id: svn://10.10.191.1:3690/sshframe/trunk@256 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ SpringStudy/src/main/java/com/promise/study/service/SpringDispatchService.java
@@ -0,0 +1,14 @@
+/*@文件名: SpringDispatchService.java  @创建人: 邢健   @创建日期: 2011-9-16 下午1:19:33*/
+package com.promise.study.service;
+
+/**   
+ * @类名: SpringDispatchService.java 
+ * @包名: com.promise.study.service 
+ * @描述: spring任务调度服务接口 
+ * @作者: 邢健 xingjian@yeah.net   
+ * @日期: 2011-9-16 下午1:19:33 
+ * @版本 V1.0   
+ */
+public interface SpringDispatchService {
+
+}
---
+++ SpringStudy/src/main/java/com/promise/study/service/impl/SpringDispatchServiceImpl.java
@@ -0,0 +1,16 @@
+/*@文件名: SpringDispatchServiceImpl.java  @创建人: 邢健   @创建日期: 2011-9-16 下午1:21:42*/
+package com.promise.study.service.impl;
+
+import com.promise.study.service.SpringDispatchService;
+
+/**   
+ * @类名: SpringDispatchServiceImpl.java 
+ * @包名: com.promise.study.service.impl 
+ * @描述: spring掉图服务接口 
+ * @作者: 邢健 xingjian@yeah.net   
+ * @日期: 2011-9-16 下午1:21:42 
+ * @版本 V1.0   
+ */
+public class SpringDispatchServiceImpl implements SpringDispatchService{
+
+}
